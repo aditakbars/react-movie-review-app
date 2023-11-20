b16833feeefbad01d7f14bd87658d0ac6dacd152
aditakbars <aditakbars33@gmail.com>
2023-11-20 21:41:22 +0700

    editing the splashscreen

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -4,6 +4,7 @@ import '../App.css'; // Pastikan kamu membuat file CSS yang sesuai
 const SplashScreen = () => {
     return (
         <div className="splash-screen">
+            <div className="loading-spinner"></div>
         <h1>My Movie Review App</h1>
         {/* Tambahkan elemen atau animasi lain sesuai kebutuhan */}
         </div>
